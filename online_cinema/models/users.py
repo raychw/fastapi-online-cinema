7e5c6aa665b24214f32f582702830b85749d0b15
raychw <bladeryz@gmail.com>
2025-02-24 00:13:38 +0200

    updated users service models to make service more secure

--- online_cinema/models/users.py
+++ online_cinema/models/users.py
@@ -40,7 +40,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String(255), unique=True, index=True, nullable=False)
-    hashed_password = Column(String(255), nullable=False)
+    _hashed_password = Column(String(255), nullable=False)
     is_active = Column(Boolean, default=True, nullable=False)
     created_at = Column(DateTime(timezone=True), 
                        server_default=func.now(), nullable=False)
