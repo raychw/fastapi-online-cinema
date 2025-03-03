5f081dabe7f98ffa12fd3c95a75fce34b7a90f02
raychw <bladeryz@gmail.com>
2025-03-03 18:50:04 +0200

    included routers to main.py

--- online_cinema/main.py
+++ online_cinema/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from routes import (
+    users
+)
+
+app = FastAPI(
+    title="Online Cinema",
+    description="API for Online Cinema",
+)
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Online Cinema API."}
+
+api_version_prefix = "/api/v1"
+
+app.include_router(users.router, prefix=f"{api_version_prefix}/users", tags=["users"])
