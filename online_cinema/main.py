0eb0e4c8f486ccfa19e20d5a3414717c29897661
raychw <bladeryz@gmail.com>
2025-03-09 12:40:29 +0200

    removed redundant tables creation from main.py

--- online_cinema/main.py
+++ online_cinema/main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
 
-from database import Base, engine
-
 app = FastAPI()
-Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"message": "Welcome to the Online Cinema API."}
