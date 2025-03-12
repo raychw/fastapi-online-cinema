f5f107607ad81642a408c158514659ecae83ac06
raychw <bladeryz@gmail.com>
2025-03-12 19:20:23 +0200

    modified main.py: added information about app and a prefix

--- online_cinema/main.py
+++ online_cinema/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from online_cinema.routers import accounts
 
+app = FastAPI(
+    title="Online Cinema API",
+    description="API for managing an online cinema application.",
+)
 
-@app.get("/")
-async def read_root():
-    return {"message": "Welcome to the Online Cinema API."}
+api_version_prefix = "/api/v1"
+
+app.include_router(accounts.router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"])
