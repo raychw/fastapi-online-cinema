dada9ab6dede6a1007caa0a4677a961cc199359a
raychw <bladeryz@gmail.com>
2025-03-16 20:40:11 +0200

    added functions for creating and verifying jtw tokens

--- online_cinema/security/utils.py
+++ online_cinema/security/utils.py
@@ -1,5 +1,34 @@
+import os
 import secrets
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
 
 
 def generate_secure_token(length: int = 32) -> str:
     return secrets.token_urlsafe(length)
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now() + expires_delta
+    else:
+        expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def verify_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
