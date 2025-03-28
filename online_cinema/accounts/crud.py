c9c85ebcb56b1f6281d31e72605b721ef2132919
raychw <bladeryz@gmail.com>
2025-03-28 11:48:04 +0200

    added crud functionality to get user by id and to get users list

--- online_cinema/accounts/crud.py
+++ online_cinema/accounts/crud.py
@@ -10,6 +10,18 @@ from online_cinema.accounts.models import (
 )
 
 
+async def get_user_by_id(db: AsyncSession, user_id: int):
+    stmt = select(UserModel).where(UserModel.id == user_id)
+    result = await db.execute(stmt)
+    return result.scalars().first()
+
+
+async def get_list_of_users(db: AsyncSession, skip: int = 0, limit: int = 10):
+    stmt = select(UserModel).offset(skip).limit(limit)
+    result = await db.execute(stmt)
+    return result.scalars().all()
+
+
 async def get_user_by_email(db: AsyncSession, email: EmailStr):
     stmt = select(UserModel).where(UserModel.email == email)
     result = await db.execute(stmt)
