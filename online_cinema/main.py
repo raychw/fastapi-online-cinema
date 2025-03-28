12ce40638df1b5d6224bf5f908b0f7ac529b130b
raychw <bladeryz@gmail.com>
2025-03-28 11:47:37 +0200

    changed app's description

--- online_cinema/main.py
+++ online_cinema/main.py
@@ -4,9 +4,9 @@ from online_cinema.routers import accounts
 
 app = FastAPI(
     title="Online Cinema API",
-    description="API for managing an online cinema application.",
+    description="API for managing an Online Cinema Application.",
 )
 
 api_version_prefix = "/api/v1"
 
-app.include_router(accounts.router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"])
+app.include_router(accounts.router, prefix=f"{api_version_prefix}/accounts", tags=["Accounts"])
